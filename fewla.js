ea613f7f2c4aa18f9738132bb612187896d9c838
Shelmiky <luidzhy@gmail.com>
2025-05-30 13:28:02 +0300

    Update fewla.js

--- fewla.js
+++ fewla.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector(".booking-form");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Спасибо за запись! Мы скоро с вами свяжемся.");
-    form.reset();
-  });
-});
+
